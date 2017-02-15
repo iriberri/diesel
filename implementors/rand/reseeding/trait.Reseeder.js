73b848450f4e54a19b1ebd77a51a496b37b62e8a
Travis CI User <travis@example.org>
2017-02-15 12:21:09 +0000

    Documentation for diesel-rs/diesel@6a00e2a

---
+++ implementors/rand/reseeding/trait.Reseeder.js
@@ -0,0 +1,17 @@
+(function() {var implementors = {};
+implementors["chrono"] = [];
+implementors["diesel"] = [];
+implementors["libc"] = [];
+implementors["num"] = [];
+implementors["quickcheck"] = [];
+implementors["rand"] = [];
+implementors["regex_syntax"] = [];
+implementors["serde"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
